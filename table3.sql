4f079a75d142d12e3f4d87078201764517802e87
Ishtishad Alam Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-01-19 14:53:41 +0600

    Create table3.sql

---
+++ table3.sql
@@ -0,0 +1,8 @@
+create table Price (purchase_price number(9) constraint pk_pp primary key,selling_price number(9));
+insert into Price values(60000,65000);
+insert into Price values(65000,70000);
+insert into Price values(70000,75000);
+insert into Price values(75000,80000);
+insert into Price values(80000,85000);
+
+select * from price;
