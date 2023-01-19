45b16aab834f866aebdf4f781f4a2006c6397280
Ishtishad Alam Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-01-19 15:25:37 +0600

    table1.sql

--- table1.sql
+++ table1.sql
@@ -1,4 +1,5 @@
 create table OWNER(c_id number(5) constraint pkc_id primary key,c_Name varchar2(20),c_Address varchar2(20));
+
 insert into OWNER values(101,'Alvi Ahmed','P.T.I,Bogura');
 insert into OWNER values(102,'Tishad Ahmed','Lotapur,Dhaka');
 insert into OWNER values(103,'Arif Ahmed','Kashbon,Natore');
